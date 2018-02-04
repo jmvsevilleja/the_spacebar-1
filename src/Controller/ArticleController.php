e40bc10ce2e599fd527a175ffe9b008b77d2501a
Bob den Otter <bobdenotter@gmail.com>
2018-02-04 11:42:01 +0100

    Workin'

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,20 +11,21 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
      */
     public function homepage()
     {
-        return new Response('OMG!');
+        return $this->render('article/homepage.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -36,8 +37,19 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
-            'comments' => $comments
+            'comments' => $comments,
+            'slug' => $slug
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        // TODO actually "do"
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 
 }
